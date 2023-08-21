0867ea27d7c96dcc39e508246f1b5bab33b65398
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-08-21 19:50:23 -0300

    criando página de registro

---
+++ frontend/planner/src/components/sign_up_form/sign_up_form.jsx
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { useState } from 'react';
+import axios from 'axios';
+
+import './sign_up_form.css'
+import '../../css/global.css'
+
+import { Link } from "react-router-dom";
+
+export default function SignUpForm() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/register', {
+        email: email,
+        username: username,
+        password: password,
+      });
+      // Add code to handle successful registration, redirect, etc.
+      setSuccess(true);
+    } catch (error) {
+      console.error('Registration error:', error);
+      // Add code to handle registration error
+    }
+  };
+
+  return (
+    <>
+    {success ? (
+                <section className='success-container'>
+                    <h1>Usuário cadastrado com sucesso!</h1>
+                    <br />
+                    <Link to="/"><Button variant="text" className="button-link">Voltar para a página principal</Button></Link>
+                </section>
+            ) : (
+    <ThemeProvider theme={createTheme()}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleRegister} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="username"
+                  label="Username"
+                  id="username"
+                  autoComplete="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+          </Box>
+          <Box sx={{ mt: 3 }}>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link to="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+    )}
+    </>
+  );
+}
--- frontend/planner/src/templates/sign_up/index.js
+++ frontend/planner/src/templates/sign_up/index.js
@@ -1,10 +1,12 @@
 import '../../css/global.css'
 import Navbar from '../../components/navbar/navbar'
+import SignUpForm from '../../components/sign_up_form/sign_up_form'
 
 function SignUp() {
   return (
     <>
       <Navbar />
+      <SignUpForm />
     </>
   );
 }
