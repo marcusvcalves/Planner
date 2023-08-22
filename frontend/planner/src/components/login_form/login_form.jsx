fdcbf097e6ba3ddbc8b66e08141134625c7780ea
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-08-22 18:29:16 -0300

    criando layout da página de login

---
+++ frontend/planner/src/components/login_form/login_form.jsx
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { useState } from 'react';
+import axios from 'axios';
+
+import './login_form.css'
+import '../../css/global.css'
+
+import { Link } from "react-router-dom";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/register', {
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        password: password,
+      });
+      // Add code to handle successful registration, redirect, etc.
+      setSuccess(true);
+    } catch (error) {
+      console.error('Registration error:', error);
+      // Add code to handle registration error
+    }
+  };
+
+  const defaultTheme = createTheme();
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container className='login_container' component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar className='lock' sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Grid container spacing={2}>
+              
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+              <Grid item xs={12}>
+              </Grid>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Lembre-se de mim"
+            />
+            <Button
+              id='btn'
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+            </Grid>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Esqueceu a senha?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Não tem uma conta? Cadastre-se"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
--- frontend/planner/src/templates/login/index.js
+++ frontend/planner/src/templates/login/index.js
@@ -1,10 +1,12 @@
 import '../../css/global.css'
 import Navbar from '../../components/navbar/navbar'
+import LoginForm from '../../components/login_form/login_form'
 
 function Login() {
   return (
     <>
       <Navbar />
+      <LoginForm />
     </>
   );
 }
