731f7e5fc8b5e8bb6d355202e9c224137b4af6fe
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-08-31 16:56:18 -0300

    Update AuthContext.js

--- frontend/planner/src/context/AuthContext.js
+++ frontend/planner/src/context/AuthContext.js
@@ -1,15 +1,15 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useContext } from 'react';
 import jwt_decode from 'jwt-decode';
+import { useNavigate } from "react-router-dom";
 
-
-const AuthContext = createContext();
-
-export default AuthContext;
+export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
 
     let [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null);
     let [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null);
+    const refresh = () => window.location.reload(true);
+    const navigate = useNavigate();
 
     let loginUser = async (e) => {
         e.preventDefault();
@@ -28,6 +28,7 @@ export const AuthProvider = ({children}) => {
             setAuthTokens(data);
             setUser(jwt_decode(data.access));
             localStorage.setItem('authTokens', JSON.stringify(data));
+            navigate('/');
         }
     }
 
@@ -35,8 +36,25 @@ export const AuthProvider = ({children}) => {
         setAuthTokens(null);
         setUser(null);
         localStorage.removeItem('authTokens');
+        refresh();
     }
     
+    let updateToken = async () => {
+        let response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({'refresh': authTokens.refresh})
+        })
+        let data = await response.json();
+
+        if (response.statusCode === 200) {
+            setAuthTokens(data);
+            setUser(jwt_decode(data.access));
+            localStorage.setItem('authTokens', JSON.stringify(data));
+        }
+    }
     let contextData = {
         user:user,
         loginUser:loginUser,
@@ -49,4 +67,8 @@ export const AuthProvider = ({children}) => {
         </AuthContext.Provider>
     )
 }
+
+export function useAuth() {
+    return useContext(AuthContext);
+  }
 
