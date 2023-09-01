f0d4a1044ccd9279e0768ea0f24df92089c8d131
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-09-01 18:19:47 -0300

    Update views.py

--- backend/planner/api/views.py
+++ backend/planner/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 from rest_framework import permissions, status
-from .validations import custom_validation, validate_email, validate_password
+from .validations import validate_email, validate_password, confirm_password
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -29,13 +29,15 @@ class MyTokenObtainPairView(TokenObtainPairView):
 class UserRegister(APIView):
 	permission_classes = (permissions.AllowAny,)
 	def post(self, request):
-		clean_data = custom_validation(request.data)
-		serializer = UserRegisterSerializer(data=clean_data)
+		data = request.data
+		serializer = UserRegisterSerializer(data=data)
+		assert confirm_password(data)
 		if serializer.is_valid(raise_exception=True):
-			user = serializer.create(clean_data)
+			user = serializer.create(serializer.validated_data)
 			if user:
 				return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class UserLogin(APIView):
