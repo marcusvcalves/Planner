3ccd376eab1e64b2315477a8825a4986eebba77a
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-09-04 15:52:13 -0300

    criando componente de tasks e pegando dados da API

--- frontend/planner/src/components/planner/planner.jsx
+++ frontend/planner/src/components/planner/planner.jsx
@@ -1,9 +1,26 @@
-import './planner.css'
-import './../../css/global.css'
+import './planner.css';
+import './../../css/global.css';
 import Button from '@mui/material/Button';
+import { TaskList } from '../task_list/task_list';
+import { useEffect, useState } from 'react';
+
+
 
 export default function Planner() {
-    return (
+
+    const [tasks, setTasks ] = useState([]);
+
+    useEffect(() =>{
+        fetch("http://127.0.0.1:8000/api/tasks/")
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setTasks(data.tasks);
+            })
+    }, []);
+    
+return (
         <section className='planner-container'>
             <div className='buttons'>
                 <Button variant='contained' className='task-button'>Nova Tarefa</Button>
@@ -19,6 +36,9 @@ export default function Planner() {
                     <p>Sexta</p>
                     <p>Sábado</p>
                     <p>Domingo</p>
+                </div>
+                <div>
+                    <TaskList tasks={tasks} />
                 </div>
             </div>
         </section>
---
+++ frontend/planner/src/components/task_list/task_list.jsx
@@ -0,0 +1,19 @@
+export const TaskList = ({ tasks }) => {
+    return (
+        <div className="task-list">
+            {tasks.map(task => (
+                <div className="columns" key={task.id}>
+                    <p className="purple">{task.time}</p>
+                    <p>{task.monday_task}</p>
+                    <p>{task.tuesday_task}</p>
+                    <p>{task.wednesday_task}</p>
+                    <p>{task.thursday_task}</p>
+                    <p>{task.friday_task}</p>
+                    <p>{task.saturday_task}</p>
+                    <p>{task.sunday_task}</p>
+                </div>
+                
+            ))}
+        </div>
+    );
+};
