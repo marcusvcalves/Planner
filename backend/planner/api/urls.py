45edb1457590f7547675c1dcac74cae030345cb9
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-09-04 21:05:39 -0300

    adicionando endpoint das tasks

--- backend/planner/api/urls.py
+++ backend/planner/api/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('user', views.UserView.as_view(), name='user'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('tasks/', views.TaskView.as_view(), name='task')
 ]
