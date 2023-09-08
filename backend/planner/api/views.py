014d46ac3b12c9fe27eaefce4bc0ac59f42ece46
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-09-08 19:39:10 -0300

    adicionando view de criação de task

--- backend/planner/api/views.py
+++ backend/planner/api/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import get_user_model, login, logout
-from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer, TaskSerializer
+from .serializers import UserRegisterSerializer, TaskSerializer, CreateTaskSerializer
 from rest_framework import permissions, status
-from .validations import validate_email, validate_password, confirm_password
+from .validations import confirm_password
 from .models import Task
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -39,43 +38,26 @@ class UserRegister(APIView):
 
 
 
-class UserLogin(APIView):
-	permission_classes = (permissions.AllowAny,)
-	authentication_classes = (SessionAuthentication,)
-	##
-	def post(self, request):
-		data = request.data
-		assert validate_email(data)
-		assert validate_password(data)
-		serializer = UserLoginSerializer(data=data)
-		if serializer.is_valid(raise_exception=True):
-			user = serializer.check_user(data)
-			login(request, user)
-			return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class UserLogout(APIView):
-	permission_classes = (permissions.AllowAny,)
-	authentication_classes = ()
-	def post(self, request):
-		logout(request)
-		return Response(status=status.HTTP_200_OK)
-
-
-class UserView(APIView):
-	permission_classes = (permissions.IsAuthenticated,)
-	authentication_classes = (SessionAuthentication,)
-	##
-	def get(self, request):
-		serializer = UserSerializer(request.user)
-		return Response({'user': serializer.data}, status=status.HTTP_200_OK)
-
-
 class TaskView(APIView):
-	serializer_class = TaskSerializer
 	permission_classes = [permissions.IsAuthenticated]
 
 	def get(self, request):
 		tasks = Task.objects.filter(user=request.user)
-		serializer = self.serializer_class(tasks, many=True)
+		serializer = TaskSerializer(tasks, many=True)
 		return Response({'tasks': serializer.data})
+
+
+class CreateTaskView(APIView):
+	permission_classes = [permissions.IsAuthenticated]
+
+	def post(self, request):
+        # Serialize os dados recebidos na requisição
+		serializer = CreateTaskSerializer(data=request.data, context={'request': request})
+		
+		if serializer.is_valid():
+			# Crie a tarefa usando o serializer
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
